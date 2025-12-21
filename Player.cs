849ee1667ff4d9c1192eab10fbc8ffcb7988c384
Valk <sebastianbelle074@protonmail.com>
2025-12-21 13:19:42 -0600

    Cleanup player script

--- Player.cs
+++ Player.cs
@@ -7,7 +7,7 @@ public partial class Player : Area2D
 
 	[Signal]
 	public delegate void HitEventHandler();
-	
+
 	[Export]
 	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
 
@@ -15,75 +15,49 @@ public partial class Player : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-			ScreenSize = GetViewportRect().Size;
-			Hide();
+		ScreenSize = GetViewportRect().Size;
+		Hide();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero; // The player's movement vector.
+		// The player's movement vector.
+		var velocity = Input.GetVector("move_left", "move_right", "move_up", "move_down"); 
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-	if (Input.IsActionPressed("move_right"))
-	{
-		velocity.X += 1;
-	}
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
 
-	if (Input.IsActionPressed("move_left"))
-	{
-		velocity.X -= 1;
-	}
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
 
-	if (Input.IsActionPressed("move_down"))
-	{
-		velocity.Y += 1;
-	}
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			// See the note below about the following boolean assignment.
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
 
-	if (Input.IsActionPressed("move_up"))
-	{
-		velocity.Y -= 1;
-	}
-
-	var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-
-	if (velocity.Length() > 0)
-	{
-		velocity = velocity.Normalized() * Speed;
-		animatedSprite2D.Play();
-	}
-	else
-	{
-		animatedSprite2D.Stop();
-	}
-	
-	Position += velocity * (float)delta;
-	Position = new Vector2(
-		x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-		y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-	);
-	
-	if (velocity.X != 0)
-	{
-		animatedSprite2D.Animation = "walk";
-		animatedSprite2D.FlipV = false;
-		// See the note below about the following boolean assignment.
 		animatedSprite2D.FlipH = velocity.X < 0;
 	}
-	else if (velocity.Y != 0)
-	{
-		animatedSprite2D.Animation = "up";
-		animatedSprite2D.FlipV = velocity.Y > 0;
-	}
-		if (velocity.X < 0)
-	{
-		animatedSprite2D.FlipH = true;
-	}
-	else
-	{
-		animatedSprite2D.FlipH = false;
-	}
-	}
-	
+
 	// We also specified this function name in PascalCase in the editor's connection window.
 	private void OnBodyEntered(Node2D body)
 	{
